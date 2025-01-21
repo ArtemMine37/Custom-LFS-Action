f2ea6b8fc09cfae0ff533a7ec98f1c7a10f279c4
ArtemMine37 <81173577+ArtemMine37@users.noreply.github.com>
2025-01-22 00:20:24 +0200

    the cleanup step didnt work (needs bash for this kind of completion)

--- env-prep.sh
+++ env-prep.sh
@@ -1,11 +1,11 @@
 # Remove unnecessary software
 echo "=> Cleaning up..."
-sudo apt-get autopurge docker-{buildx-plugin,ce{,-cli}} containerd.io podman buildah skopeo containers-common kubectl \
+bash -c "sudo apt-get autopurge docker-{buildx-plugin,ce{,-cli}} containerd.io podman buildah skopeo containers-common kubectl \
 adoptium-ca-certificates {openjdk,adoptopenjdk,temurin,nginx,apache2,php,vim,{,lib}mono,gfortran,postgresql,libgtk-3,ant{,-optional} \
 libpq-dev libmysqlclient* msodbcsql* mssql-tools unixodbc-dev mysql-client* mysql-common mysql-server* php*-*sql sphinxsearch mongodb* \
 firefox {google-chrome,microsoft-edge}-stable xvfb apache2 nginx php{7,8}* session-manager-plugin {azure,google-cloud}-cli heroku subversion \
 mercurial vim dotnet* aspnetcore* mono{doc}* libmono* msbuild nuget ruby* rake ri powershell r-{base,cran,doc}* r-recommended snapd man{-db,pages} \
-ubuntu-mono *-icon-theme esl-erlang imagemagick{,-6-common} libgl1-mesa-dri firebird* hhvm >> /dev/null
+ubuntu-mono *-icon-theme esl-erlang imagemagick{,-6-common} libgl1-mesa-dri firebird* hhvm"
 echo "=> Preparing software..."
 sudo apt-get update
 sudo apt-get upgrade

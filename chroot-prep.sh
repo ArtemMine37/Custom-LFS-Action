c7c982c2e149bddbd9534cc22804e7276d4bcb55
ArtemMine37 <81173577+ArtemMine37@users.noreply.github.com>
2025-01-28 00:27:37 +0200

    Create chroot-prep.sh

---
+++ chroot-prep.sh
@@ -0,0 +1,76 @@
+# Preparing. Just the preparing.
+export TARBALLS=$LFS/sources
+cd $TARBALLS
+# Unpacking most sources
+tar -xf binutils-2.43.1.tar.xz
+tar -xf gcc-14.2.0.tar.xz
+tar -xf linux-6.6.74.tar.xz
+# Build binutils first
+cd binutils-2.43.1
+mkdir -v build
+cd build
+../configure --prefix=$LFS/tools \
+             --with-sysroot=$LFS \
+             --target=$LFS_TGT   \
+             --disable-nls       \
+             --enable-gprofng=no \
+             --disable-werror    \
+             --enable-new-dtags  \
+             --enable-default-hash-style=gnu
+make
+# and install
+make install
+# Now, let's compile GCC for the first time
+cd ../../gcc-14.2.0
+# Some dependencies and tricks
+tar -xf ../mpfr-4.2.1.tar.xz
+mv -v mpfr-4.2.1 mpfr
+tar -xf ../gmp-6.3.0.tar.xz
+mv -v gmp-6.3.0 gmp
+tar -xf ../mpc-1.3.1.tar.gz
+mv -v mpc-1.3.1 mpc
+
+case $(uname -m) in
+  x86_64)
+    sed -e '/m64=/s/lib64/lib/' \
+        -i.orig gcc/config/i386/t-linux64
+ ;;
+esac
+# Let's build!
+mkdir -v build
+cd build
+
+../configure                  \
+    --target=$LFS_TGT         \
+    --prefix=$LFS/tools       \
+    --with-glibc-version=2.40 \
+    --with-sysroot=$LFS       \
+    --with-newlib             \
+    --without-headers         \
+    --enable-default-pie      \
+    --enable-default-ssp      \
+    --disable-nls             \
+    --disable-shared          \
+    --disable-multilib        \
+    --disable-threads         \
+    --disable-libatomic       \
+    --disable-libgomp         \
+    --disable-libquadmath     \
+    --disable-libssp          \
+    --disable-libvtv          \
+    --disable-libstdcxx       \
+    --enable-languages=c,c++
+make
+# and install
+make install
+cd ..
+cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
+  `dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/include/limits.h
+# Linux headers
+cd ../linux-6.6.74
+make mrproper
+make headers
+find usr/include -type f ! -name '*.h' -delete
+cp -rv usr/include $LFS/usr
+cd ..
+echo "[seemed to be OK]"

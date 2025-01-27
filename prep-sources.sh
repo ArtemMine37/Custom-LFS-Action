cae93942c805543b3cd1691351fbb8ec3254d20b
ArtemMine37 <81173577+ArtemMine37@users.noreply.github.com>
2025-01-28 00:34:34 +0200

    comments?

--- prep-sources.sh
+++ prep-sources.sh
@@ -4,3 +4,5 @@ mkdir -v $LFS/sources # Make the directory
 chmod -v a+wt $LFS/sources
 wget --input-file=download --continue --directory-prefix=$LFS/sources # Needs wget to be installed
 echo "  [OK]" # no package verification - only hardcore
+# Chroot-prep
+echo "   [OK]"

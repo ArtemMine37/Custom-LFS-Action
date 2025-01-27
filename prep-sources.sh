52fe1f4bb3612a07ca36f15a0a3477bc6d41c1f8
ArtemMine37 <81173577+ArtemMine37@users.noreply.github.com>
2025-01-27 23:13:53 +0200

    Create prep-sources.sh

---
+++ prep-sources.sh
@@ -0,0 +1,6 @@
+export LFS="/mnt/lfs" # Prepare the LFS directory
+echo "=> Downloading package sources..."
+mkdir -v $LFS/sources # Make the directory
+chmod -v a+wt $LFS/sources
+wget --input-file=download --continue --directory-prefix=$LFS/sources # Needs wget to be installed
+echo "  [OK]" # no package verification - only hardcore
